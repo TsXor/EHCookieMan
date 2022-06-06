109d598acb973f8fc36c18d146c7ac6d1deff0b9
TsXor <74352334+TsXor@users.noreply.github.com>
2022-06-06 23:38:52 +0800

    Update EHCookieMan.js

--- EHCookieMan.js
+++ EHCookieMan.js
@@ -9,8 +9,8 @@
 // @grant        none
 // ==/UserScript==
 //you can convert the compressed part of the script to human-readable from in https://c.runoob.com/front-end/51/
-function setCookie(cname,cvalue,exdays){var d=new Date();d.setTime(d.getTime()+(exdays*24*60*60*1000));var expires="expires="+d.toGMTString();document.cookie=cname+"="+cvalue+"; "+expires+"; SameSite=None; Domain=.exhentai.org"}function getCookie(cname){var name=cname+"=";var ca=document.cookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i].trim();if(c.indexOf(name)==0)return c.substring(name.length,c.length)}return""}function delCookie(cname){setCookie(cname,"",-1)}
 var cookiefunc_script_text='function setCookie(cname,cvalue,exdays){var d=new Date();d.setTime(d.getTime()+(exdays*24*60*60*1000));var expires="expires="+d.toGMTString();document.cookie=cname+"="+cvalue+"; "+expires+"; SameSite=None; Domain=.exhentai.org"}function getCookie(cname){var name=cname+"=";var ca=document.cookie.split(\';\');for(var i=0;i<ca.length;i++){var c=ca[i].trim();if(c.indexOf(name)==0)return c.substring(name.length,c.length)}return""}function delCookie(cname){setCookie(cname,"",-1)}'
+eval(cookiefunc_script_text)
 var cookiefunc_script = document.createElement("script");
 cookiefunc_script.type = "text/javascript";
 cookiefunc_script.appendChild(document.createTextNode(cookiefunc_script_text));

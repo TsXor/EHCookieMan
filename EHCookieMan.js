5bd68f35084431881591e32a4cac6d0a6f76de72
TsXor <74352334+TsXor@users.noreply.github.com>
2022-06-07 00:35:11 +0800

    Update EHCookieMan.js

--- EHCookieMan.js
+++ EHCookieMan.js
@@ -8,7 +8,7 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-//you can convert the compressed part of the script to human-readable from in https://c.runoob.com/front-end/51/
+//you can convert the compressed part of the script to human-readable form in https://c.runoob.com/front-end/51/
 var cookiefunc_script_text='function setCookie(cname,cvalue,exdays){var d=new Date();d.setTime(d.getTime()+(exdays*24*60*60*1000));var expires="expires="+d.toGMTString();document.cookie=cname+"="+cvalue+"; "+expires+"; SameSite=None; Domain=.exhentai.org"}function getCookie(cname){var name=cname+"=";var ca=document.cookie.split(\';\');for(var i=0;i<ca.length;i++){var c=ca[i].trim();if(c.indexOf(name)==0)return c.substring(name.length,c.length)}return""}function delCookie(cname){setCookie(cname,"",-1)}'
 eval(cookiefunc_script_text)
 var cookiefunc_script = document.createElement("script");
